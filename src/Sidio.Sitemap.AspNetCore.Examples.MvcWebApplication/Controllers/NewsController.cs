8f7dca9a9bd4b5fb179e97d93a9ec50fa523c344
Marthijn van den Heuvel <marthijn@sidio.nl>
2024-03-22 16:01:07 +0100

    📝 Added examples

---
+++ src/Sidio.Sitemap.AspNetCore.Examples.MvcWebApplication/Controllers/NewsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sidio.Sitemap.AspNetCore.Examples.MvcWebApplication.Controllers;
+
+public sealed class NewsController : Controller
+{
+    public string Article1()
+    {
+        return "article1";
+    }
+}
--- src/Sidio.Sitemap.AspNetCore.Examples.MvcWebApplication/Controllers/SitemapController.cs
+++ src/Sidio.Sitemap.AspNetCore.Examples.MvcWebApplication/Controllers/SitemapController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Sidio.Sitemap.Core;
+using Sidio.Sitemap.Core.Extensions;
 
 namespace Sidio.Sitemap.AspNetCore.Examples.MvcWebApplication.Controllers;
 
@@ -8,10 +9,13 @@ public sealed class SitemapController : Controller
     [Route("sitemap.xml")]
     public IActionResult SitemapIndex()
     {
-        var sitemap = new SitemapIndex();
-        sitemap.Add(new SitemapIndexNode(Url.Action("SitemapHome")));
+        var sitemapIndex = new SitemapIndex();
+        sitemapIndex.Add(new SitemapIndexNode(Url.Action("SitemapHome")));
+        sitemapIndex.Add(new SitemapIndexNode(Url.Action("SitemapNews")));
+        sitemapIndex.Add(new SitemapIndexNode(Url.Action("SitemapImages")));
+        sitemapIndex.Add(new SitemapIndexNode(Url.Action("SitemapVideos")));
 
-        return new SitemapResult(sitemap);
+        return new SitemapResult(sitemapIndex);
     }
 
     [Route("sitemap-home.xml")]
@@ -20,6 +24,37 @@ public sealed class SitemapController : Controller
         var sitemap = new Core.Sitemap();
         sitemap.Add(new SitemapNode(Url.Action("Index", "Home")));
         sitemap.Add(new SitemapNode(Url.Action("Privacy", "Home")));
+
+        return new SitemapResult(sitemap);
+    }
+
+    [Route("sitemap-news.xml")]
+    public IActionResult SitemapNews()
+    {
+        var sitemap = new Core.Sitemap();
+        sitemap.Add(new SitemapNewsNode(Url.Action("Article1", "News"), "Article1", "John Doe", "EN", DateTime.UtcNow));
+
+        return new SitemapResult(sitemap);
+    }
+
+    [Route("sitemap-images.xml")]
+    public IActionResult SitemapImages()
+    {
+        var imageLocation = new ImageLocation("non-existing-image.jpg");
+
+        var sitemap = new Core.Sitemap();
+        sitemap.Add(new SitemapImageNode(Url.Action("Index", "Home"), imageLocation));
+
+        return new SitemapResult(sitemap);
+    }
+
+    [Route("sitemap-videos.xml")]
+    public IActionResult SitemapVideos()
+    {
+        var video = new VideoContent("non-existing-video-thumbnail.jpg", "Video1", "Video1 description", "non-existing-video.mp4", null);
+
+        var sitemap = new Core.Sitemap();
+        sitemap.Add(new SitemapVideoNode(Url.Action("Index", "Home"), video));
 
         return new SitemapResult(sitemap);
     }
