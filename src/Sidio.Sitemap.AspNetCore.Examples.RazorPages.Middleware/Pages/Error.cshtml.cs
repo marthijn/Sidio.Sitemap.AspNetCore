f68618b2fac09a6bfaa44d7bfa1e4db0529d2e76
Marthijn van den Heuvel <marthijn@sidio.nl>
2025-11-12 08:14:43 +0100

    Add [ExcludeFromCodeCoverage] attribute to Error, Index, and Privacy page models

--- src/Sidio.Sitemap.AspNetCore.Examples.RazorPages.Middleware/Pages/Error.cshtml.cs
+++ src/Sidio.Sitemap.AspNetCore.Examples.RazorPages.Middleware/Pages/Error.cshtml.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Sidio.Sitemap.AspNetCore.Examples.RazorPages.Middleware.Pages
 {
+    [ExcludeFromCodeCoverage]
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
--- src/Sidio.Sitemap.AspNetCore.Examples.RazorPages.Middleware/Pages/Index.cshtml.cs
+++ src/Sidio.Sitemap.AspNetCore.Examples.RazorPages.Middleware/Pages/Index.cshtml.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Sidio.Sitemap.AspNetCore.Examples.RazorPages.Middleware.Pages;
 
+[ExcludeFromCodeCoverage]
 public class IndexModel : PageModel
 {
     public void OnGet()
